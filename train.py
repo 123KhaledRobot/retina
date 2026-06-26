34d181ecf5eb64bd723cec9b167e7f2b3c3d184a
123KhaledRobot <rckhaled69@gmail.com>
2026-06-26 14:43:05 +0300

    ResNet50 done. deploying to Kaggle

--- train.py
+++ train.py
@@ -7,6 +7,15 @@ from model import model
 
 import os
 
+from sklearn.metrics import cohen_kappa_score
+#this we need for model evaluation
+from sklearn.model_selection import train_test_split
+#this for stratified split feature
+
+from torch.utils.data import Subset
+# we need this to later make the train,cv,test datasets 
+# from the stratified dataset 
+
 """
 this means that we are importing ResNet50 which we have
 placed under the variable model from model.py
@@ -39,15 +48,44 @@ and hence we get the sampled df
 sample_csv=sample_df.to_csv("sample.csv", index=False)
 
 dataset=DR("sample.csv","sample/")
-train_dataset, val_dataset, test_dataset = random_split(dataset, [6, 1, 3])
 
-dataloader=DataLoader(train_dataset,batch_size=2,shuffle=True)
+# train_dataset, val_dataset, test_dataset = random_split(dataset, [6, 1, 3])
+"""
+this random_split done was only for sample testing
 
-val_loader=DataLoader(val_dataset,batch_size=2,shuffle=False)
+it won't be justified on a class imbalanced dataset. 
+to fix that, we use stratified split
+"""
 
-test_loader=DataLoader(test_dataset,batch_size=2,shuffle=False)
+labels=[dataset[i][1] for i in range(len(dataset))]
+indices = list(range(len(dataset)))
 
-num_epochs=5
+trainval_idx, test_idx = train_test_split(
+    indices, test_size=0.3, stratify=labels, random_state=42
+)
+#this splits train+crossval and testing into 7:3 ratio
+#since we wanted 30% testing data
+
+train_idx, val_idx = train_test_split(
+    trainval_idx, test_size=0.142857, stratify=[labels[i] for i in trainval_idx], random_state=42
+)
+
+#then we do the same approximately to make it 6:1 ratio
+
+train_dataset=Subset(dataset,train_idx)
+val_dataset=Subset(dataset,val_idx)
+test_dataset=Subset(dataset,test_idx)
+
+dataloader=DataLoader(train_dataset,batch_size=64,shuffle=True)
+
+val_loader=DataLoader(val_dataset,batch_size=64,shuffle=False)
+
+test_loader=DataLoader(test_dataset,batch_size=64,shuffle=False)
+
+num_epochs=20 #before it was five since we were running on sample
+
+#and batch_size is changed to 64 from 2 since the dataset is massive
+
 """
 what does .train do ?
 
@@ -107,8 +145,19 @@ all_labels=[]
 with torch.no_grad():
     for images, labels in test_loader:
         preds = torch.argmax(model(images), dim=1)
-        #here argmax
+        #here argmax returns a tensor [batch_sz]
+        # for eg: if batch_sz is 2, then there are two classes. 
+        # one predicted class per image
 
+        all_preds.extend(preds.tolist())
+        #tolist() convers a tensor to a list
+        all_labels.extend(labels.tolist())
+
+
+
+kappa = cohen_kappa_score(all_labels, all_preds, weights='quadratic')
+
+print(f"Test QWK: {kappa:.4f}")
 
 """
 what does .eval mean?
